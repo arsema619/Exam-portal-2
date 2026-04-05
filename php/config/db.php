7fa295f5857c948ea27fa1a27c6e525caf4320b5
Azeb Yirga <azyirga@gmail.com>
2026-04-05 22:53:11 +0600

    Switch database connection from MySQLi to PDO

--- php/config/db.php
+++ php/config/db.php
@@ -4,15 +4,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
-
-$conn = mysqli_connect(
-    $_ENV['DB_HOST'],
+try{
+$conn = new PDO (
+   "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8mb4",
     $_ENV['DB_USER'],
-    $_ENV['DB_PASS'],
-    $_ENV['DB_NAME']
-);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    $_ENV['DB_PASS']
+   
+);   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+    die("Connection failed: " .$e->getMessage());
 }
 ?>
